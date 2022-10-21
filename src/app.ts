49e671ed5988fffc4759cea624e8e5add8a263dd
Petra <pasztor.petra1998@gmail.com>
2022-10-21 15:13:24 +0200

    kész

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 // Importáld be a HumanHero és TransformerHero osztályokat.
-
+import { HumanHero } from "./hero";
+import { TransformerHero } from "./transformer";
 
 /**
  * Állítsd be helyesen a változót!
@@ -8,7 +9,11 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: any[] = [
+  new HumanHero(1 ,"Anna", "female", 34, 90),
+  new HumanHero(2, "András", "male", 21, 98),
+  new HumanHero(3, "Andrea", "female", 40, 97),
+];
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +22,8 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} transformers
  */
-export const transformers: any[] = [];
+export const transformers: any[] = [
+  new TransformerHero (1, "Balázs", 3, 4, "clan1"),
+  new TransformerHero (2, "Betti", 6, 2, "clan2"),
+  new TransformerHero (3, "Béla", 2, 8, "clan3"),
+];
--- src/hero.ts
+++ src/hero.ts
@@ -7,6 +7,32 @@ import { IHuman } from './hero-interface';
  * Deklaráld benne az interfész által megkövetelt változókat.
  * Definiáld a konstruktort és kérj be minden változót, majd állítsd be őket.
  */
-export class HumanHero {
+export class HumanHero implements IHuman {
+  id: number;
+  name: string;
+  sex: string;
+  age: number;
+  health: number;
+  photo?: string;
 
+  constructor(id: number, name: string, sex: string, age: number, health: number, photo?: string) {
+    this.id = id;
+    this.name = name;
+    this.sex = sex;
+    this.age = age;
+    this.health = health;
+    this.photo = photo;
+  }
 }
+
+// export class HumanHero implements IHuman {
+//   constructor(
+//     public sex: string,
+//     public age: number,
+//     public health: number,
+//     public id: number,
+//     public name: string,
+//     public photo?: string,
+//   ) {}
+// }
+
